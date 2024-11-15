fb94a1ba580ecb4c2ee266ab2a62e28309f63178
Aimylys <denaison.emilie@gmail.com>
2024-11-15 09:49:35 +0100

    first commit

---
+++ src/main/java/com/ac/kappasisland/KappasIslandApplication.java
@@ -0,0 +1,13 @@
+package com.ac.kappasisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KappasIslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KappasIslandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ac/kappasisland/KappasIslandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.kappasisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KappasIslandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
